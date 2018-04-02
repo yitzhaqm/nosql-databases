301320248668be9a49678c302e74ad944ada44cb
YitzhaqM <yishaktofik@gmail.com>
2018-04-02 16:17:30 -0400

    Homework 1 completed; homework_1.py

---
+++ redis/homework_1.py
@@ -0,0 +1,11 @@
+import httplib
+import json
+
+def makeRequest():
+	ren = httplib.HTTPSConnection(host="api.nasa.gov", port=None)
+	req = ren.request(method="GET", url="/planetary/apod?date=1998-08-19&api_key=z0cVI1CIPWs4oceOoy4DlApuWtVfV5BN7d8MWEmw")
+	res = ren.getresponse()
+	obj = json.load(res)
+	print(obj["url"])
+
+makeRequest()
